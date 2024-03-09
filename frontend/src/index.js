73f3af73f1f8883e6725944dcdc5af453f5cb7f4
Adi-The-Pro <98386784+Adi-The-Pro@users.noreply.github.com>
2024-03-09 23:15:06 +0530

    backend url updated in backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {Toaster} from 'react-hot-toast'
 
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'https://scalar-deployment.vercel.app';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
